25d5c606a0828886bec1e00fa78653d24ba11776
taehunlim <dsa1520@naver.com>
2022-11-30 20:11:50 +0900

    Feat: jest image handling

--- jest.config.ts
+++ jest.config.ts
@@ -1,5 +1,10 @@
-export = {
-    "setupFilesAfterEnv": [
-        "<rootDir>/src/setupTest.ts"
-    ]
-}
+export default {
+  modulePaths: ["node_modules", "<rootDir>/src"],
+  setupFilesAfterEnv: ["<rootDir>/src/setupTest.ts"],
+  coveragePathIgnorePatterns: ["test/.^&"],
+  moduleNameMapper: {
+    "^!raw-loader!.*": "jest-raw-loader",
+    "\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$":
+      "<rootDir>/src/utils/test/assetsTransformer.js",
+  },
+};
